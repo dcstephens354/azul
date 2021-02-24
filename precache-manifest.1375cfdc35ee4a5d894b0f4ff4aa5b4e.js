707099b608df3735cc40802a1ff08a78153eb4cf
dcstephens354 <79426054+dcstephens354@users.noreply.github.com>
2021-02-23 20:35:16 -0500

    Updates

---
+++ precache-manifest.1375cfdc35ee4a5d894b0f4ff4aa5b4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81c36827473ff676af1013837d88a4e0",
+    "url": "/azul/index.html"
+  },
+  {
+    "revision": "2db3d6b56b8f107beb50",
+    "url": "/azul/static/css/main.88df7bc5.chunk.css"
+  },
+  {
+    "revision": "245e92aa3c50cca564b3",
+    "url": "/azul/static/js/2.5e69fd62.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/azul/static/js/2.5e69fd62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2db3d6b56b8f107beb50",
+    "url": "/azul/static/js/main.a8f470d3.chunk.js"
+  },
+  {
+    "revision": "62bc6a6a1b7ffc1ac72f",
+    "url": "/azul/static/js/runtime-main.cb87e6c6.js"
+  },
+  {
+    "revision": "bd4e2b8b4ddbb29a8c76026b02db8c12",
+    "url": "/azul/static/media/Circle_Tan_Solid2.bd4e2b8b.png"
+  }
+]);
